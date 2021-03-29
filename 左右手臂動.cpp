c184471f5fa928714dad9a645c597b9a9b172ea7
Jerrymcu <80246773+Jerrymcu@users.noreply.github.com>
2021-03-29 15:31:01 +0800

    Add files via upload

---
+++ 左右手臂動.cpp
@@ -0,0 +1,51 @@
+#include <GL/glut.h> ///(0) 使用GLUT外掛 (忘了裝 GLUT)
+float angle=0; ///TODO:
+void hand()
+{
+    glPushMatrix();
+        glScalef(0.5,0.1,0.1);
+        glutSolidCube(1);
+    glPopMatrix();
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);///清空
+    glPushMatrix();///左半邊
+        glTranslatef(-0.25,0,0);
+        glRotatef(angle,0,0,1);
+        glTranslatef(-0.25,0,0);
+        hand();///上
+        glPushMatrix(); ///TODO: 還原矩陣
+            glTranslatef(-0.25,0,0);
+            glRotatef(angle,0,0,1);
+            glTranslatef(-0.25,0,0);
+            hand();///下手臂
+        glPopMatrix();
+    glPopMatrix();
+
+    glPushMatrix();///右半邊
+        glTranslatef(+0.25,0,0);
+        glRotatef(-angle,0,0,1);
+        glTranslatef(+0.25,0,0);
+        hand();///上
+        glPushMatrix(); ///TODO: 還原矩陣
+            glTranslatef(+0.25,0,0);
+            glRotatef(-angle,0,0,1);
+            glTranslatef(+0.25,0,0);
+            hand();///下
+        glPopMatrix();
+    glPopMatrix();
+
+    glutSwapBuffers();
+    angle++;
+}
+int main(int argc, char ** argv)
+{
+    glutInit( &argc, argv ); ///(1) GLUT初始設定
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);///(2)顯示模式
+    glutCreateWindow("Jerrychen");///(3)開窗
+
+    glutIdleFunc(display);///(4)等一下要顯示的函式
+    glutDisplayFunc( display );///TODO: 按按鍵, 它就會轉動!!!!
+    glutMainLoop();///(5)主要迴圈
+}
