c7219af4d99a6eb2c62bbe65b0445d96a12a60a1
Jerrymcu <80246773+Jerrymcu@users.noreply.github.com>
2021-05-03 15:36:15 +0800

    燈光

--- main.cpp
+++ main.cpp
@@ -1,28 +1,59 @@
-#include <GL/glut.h>///以下請在blog自己copy
-#include <stdio.h>///TODO:
-float teapotX=0,teapotY=0;
+#include "glm.h"
+GLMmodel*pmodel=NULL;
+void drawmodel(void)
+{
+    if (!pmodel) {
+	pmodel = glmReadOBJ("data/porsche.obj");
+	if (!pmodel) exit(0);
+	glmUnitize(pmodel);
+	glmFacetNormals(pmodel);
+	glmVertexNormals(pmodel, 90.0);
+    }
+
+    glmDraw(pmodel, GLM_SMOOTH | GLM_MATERIAL);
+}
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();
+        glRotatef(180,0,1,0);
+        drawmodel();
     glPopMatrix();
-        glTranslatef(teapotX,teapotY,0);
-        glutSolidTeapot(0.3);
-    glPopMatrix();
-    glEnd();
     glutSwapBuffers();
 }
-void motion(int x,int y)///
+    const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
+    const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+    const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    const GLfloat light_position[] = { 2.0f, 5.0f, -5.0f, 0.0f };
+
+    const GLfloat mat_ambient[ ]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+    const GLfloat mat_diffuse[ ]    = { 0.8f, 0.8f, 0.8f, 1.0f };
+    const GLfloat mat_specular[ ]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+    const GLfloat high_shininess[ ] = { 100.0f };
+
+int main(int argc,char**argv)
 {
-    teapotX=(x-150)/150.0;
-    teapotY=-(y-150)/150.0;
-    display();
-}///TODO:印出來...
-int main(int argc,char **argv)
-{
-    glutInit(&argc, argv);
+    glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("week04 mouse");
+    glutCreateWindow("week11");
+    glEnable(GL_DEPTH_TEST);    ///打光開始
+    glDepthFunc(GL_LESS);
+
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHTING);
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);    ///打光結束
+
     glutDisplayFunc(display);
-    glutMotionFunc( motion );
     glutMainLoop();
 }
